aa3763ef823dee250c10a8f8993ae228a36a5e29
Ali93reza <a.r1993@gmail.com>
2024-04-16 13:42:53 +0000

    add class imbalance visualisation.

---
+++ Visualise.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import graphviz
+import numpy as np
+import pandas as pd
+import os
+import pickle
+
+OUTPUT_FOLDER = "./out/"
+
+with open(os.path.join(OUTPUT_FOLDER, "model.pkl"), "rb") as f:
+    [train_features, train_labels, test_features, test_labels, model] = (
+        pickle.load(f)
+    )
+
+
+def vis_class_imbalance(labels):
+    autopct = "%.1f"
+    labels_df = pd.DataFrame({"target": labels})
+    labels_df.value_counts().plot.pie(autopct=autopct)
+    plt.show()
+
